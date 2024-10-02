45cbb4c5cee3f903469fe555002ff28149abd0d5
Isaias Fernandes <70554061+IsaiasFernandes22@users.noreply.github.com>
2024-10-01 22:18:12 -0300

    delete all

--- criacao_de_api_para_criacao_e_edicao_de_conteudo-rawenhyo/Models/Notification.cs
+++
@@ -1,8 +0,0 @@
-public class Notification
-{
-    public int Id { get; set; }
-    public string Message { get; set; }
-    public string Link { get; set; }
-    public bool IsRead { get; set; }
-    public string RecipientEmail { get; set; }
-}
