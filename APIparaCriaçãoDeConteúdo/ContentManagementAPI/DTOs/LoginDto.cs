a26a94dc1eb7d242ac6a4c7f62998b9bf5fed3ad
Isaias Fernandes <70554061+IsaiasFernandes22@users.noreply.github.com>
2024-10-09 20:27:39 -0300

    Delete APIparaCriaçãoDeConteúdo directory

--- APIparaCriaçãoDeConteúdo/ContentManagementAPI/DTOs/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-// C:\algoread-backend1\APIparaCriaçãoDeConteúdo\ContentManagementAPI\DTOs\LoginDto.cs
-namespace ContentManagementAPI.DTOs
-{
-    public class LoginDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- APIparaCriaçãoDeConteúdo/ContentManagementAPI/DTOs/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-// C:\algoread-backend1\APIparaCriaçãoDeConteúdo\ContentManagementAPI\DTOs\RegisterDto.cs
-namespace ContentManagementAPI.DTOs
-{
-    public class RegisterDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
