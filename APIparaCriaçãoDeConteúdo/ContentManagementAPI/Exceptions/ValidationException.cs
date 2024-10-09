9fb789c256779c700de2d0e027b7a6eb1407333e
Isaias Fernandes <70554061+IsaiasFernandes22@users.noreply.github.com>
2024-10-08 22:42:05 -0300

    Delete APIparaCriaçãoDeConteúdo directory

--- APIparaCriaçãoDeConteúdo/ContentManagementAPI/Exceptions/ValidationException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ContentManagementAPI.Exceptions
-{
-    public class ValidationException : Exception
-    {
-        public ValidationException(string message) : base(message) { }
-    }
-}
--- APIparaCriaçãoDeConteúdo/ContentManagementAPI/Models/Role.cs
+++
@@ -1,9 +0,0 @@
-namespace ContentManagementAPI.Models
-{
-    public class Role
-    {
-        public string Name { get; set; } = string.Empty; 
-
-        public Role() { }
-    }
-}
